aa012dc0023c5b84cd9774b28ee68f98b6117092
AhmadElsaeed <75431123+AhmadElsaeedd@users.noreply.github.com>
2023-10-16 11:52:05 +0400

    Initial Commit

---
+++ nodistance/nodistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  nodistanceApp.swift
+//  nodistance
+//
+//  Created by NYUAD on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct nodistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
